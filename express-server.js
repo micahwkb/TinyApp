992e25490aad5a524392f5bd7acc5fbef27ff2bf
Micah Britton <micahwkb@gmail.com>
2016-11-08 18:35:57 -0500

    added working redirect via app.get in the /u/: space

--- express-server.js
+++ express-server.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 
-const bodyParser = require("body-parser");
-app.user(bodyParser.urlencoded({extended: true}));
-
 const app = express();
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 8080;
 
@@ -15,7 +15,6 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
   };
-
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -26,13 +25,20 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-});
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+// below would catch /new if not placed below
 
+// app.get("/urls/:id", (req, res) => {
+//   let templateVars = { shortURL: req.params };
+//   console.log(templateVars)
+// });
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL)
+})
 app.post("/urls", (req, res) => {
   console.log(req.body); // log POST parameters for debugging
   res.send("OK"); // (to be changed later)
